0ff207d12ac4b60b235f44cf9e11cc3eaaff25a9
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-04-17 11:36:37 +0600

    private route created

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
 import Header from "./Pages/Shared/Header/Header";
 import Blogs from "./Pages/Blogs/Blogs";
-import About from "./Pages/About/About"
-
+import About from "./Pages/About/About";
+import RequireAuth from "./Pages/Auth/RequireAuth/RequireAuth";
+import Checkout from "./Pages/Checkout/Checkout";
 
 function App() {
   return (
@@ -16,16 +17,16 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/about" element={<About></About>}></Route>
-    {/*     <Route path="/login" element={<Login></Login>}></Route>
+        {/*     <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signup" element={<SignUp></SignUp>}></Route> */}
-  {/*       <Route
+        <Route
           path="/test"
           element={
             <RequireAuth>
-              <Test></Test>
+              <Checkout></Checkout>
             </RequireAuth>
           }
-        ></Route> */}
+        ></Route>
       </Routes>
     </div>
   );

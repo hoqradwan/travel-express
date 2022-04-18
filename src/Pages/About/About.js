8bc2a27d019b14901f56695ab39936bec445ef84
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-04-18 23:47:27 +0600

    about section edited

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./About.css";
-import about from "../../images/about.jpg"
+import about from "../../images/about.jpg";
 
 const About = () => {
   return (
-    <div className="about-container">
+    <div className="about-container d-flex flex-column align-items-center">
       <h2 className="about text-center">About Me</h2>
-      <div className="about-img">
-      <img src={about} alt="" />
-
+      <div className="mt-2">
+        <img src={about} alt="" />
       </div>
-      <h5 className="fs-3 text-center mt-5">Md. Radwanul Hoque</h5>
+      <h5 className="fs-3 text-center mt-3">Md. Radwanul Hoque</h5>
       <p>
         I am Md. Radwanul Hoque. My goal is to be a professional full stack web
         developer. I want to serve my clients with efficient and quality
